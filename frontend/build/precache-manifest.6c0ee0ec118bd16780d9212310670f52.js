1b3939af35113684140e4bb7839c5481a6a2a243
daniel mckee <danielmckee@daniels-mini.home>
2023-03-10 15:50:05 +0000

    Vercel config

---
+++ frontend/build/precache-manifest.6c0ee0ec118bd16780d9212310670f52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db266a570fbcfd5971437c95737216fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa051dae575dda5c7f29",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c8fc9b97165d21b68ce1",
+    "url": "/static/js/2.0485771d.chunk.js"
+  },
+  {
+    "revision": "aa051dae575dda5c7f29",
+    "url": "/static/js/main.8c0787cc.chunk.js"
+  },
+  {
+    "revision": "df983d35010a056c2c56",
+    "url": "/static/js/runtime~main.6c9058b0.js"
+  }
+]);
